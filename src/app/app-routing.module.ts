cfcb55bcc4ef4a2e4ddccd9e2011792b5d7d69c0
Thomas Setiabudi <thomsetiabudi@gmail.com>
2020-06-21 12:52:17 +0700

    remove unused pages
    remove unused pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { KisahComponent } from './kisah/kisah.component';
 import { FacerecogComponent } from './facerecog/facerecog.component';
-import { GembalaComponent } from './gembala/gembala.component';
-import { PersekutuanComponent } from './persekutuan/persekutuan.component';
-import { PersekutuanConnectComponent } from './persekutuan-connect/persekutuan-connect.component';
-import { PersekutuanPwComponent } from './persekutuan-pw/persekutuan-pw.component';
-import { KomunitasComponent } from './komunitas/komunitas.component';
-import { KomunitasKidsComponent } from './komunitas-kids/komunitas-kids.component';
-import { KomunitasPremiumComponent } from './komunitas-premium/komunitas-premium.component';
-import { KomunitasTeensComponent } from './komunitas-teens/komunitas-teens.component';
-import { KomunitasYouthComponent } from './komunitas-youth/komunitas-youth.component';
-import { KomunitasDemudComponent } from './komunitas-demud/komunitas-demud.component';
-import { KomunitasExcellentComponent } from './komunitas-excellent/komunitas-excellent.component';
-import { VisiComponent } from './visi/visi.component';
 
 const routes: Routes = [
   {
@@ -34,139 +21,11 @@ const routes: Routes = [
     }
   },
   {
-    path: 'kisah', component: KisahComponent,
-    data: {
-      title: 'Sejarah Web CheckIn',
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/kisah'
-    }
-  },
-  {
     path: 'facerecog', component: FacerecogComponent,
     data: {
       title: 'Face Recognition',
       description: 'Kenali wajah anda sendiri',
       ogUrl: 'https://checkints.web.app/facerecog'
-    }
-  },
-  {
-    path: 'kisah/visi', component: VisiComponent,
-    data: {
-      title: 'Visi Web CheckIn',
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/kisah/visi'
-    }
-  },
-  {
-    path: 'kisah/sejarah', component: KisahComponent,
-    data: {
-      title: 'Sejarah Web CheckIn',
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/kisah'
-    }
-  },
-  {
-    path: 'kisah/gembala', component: GembalaComponent,
-    data: {
-      title: 'Gembala Web CheckIn Pdt. Febrian',
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/kisah/gembala'
-    }
-  },
-  {
-    path: 'kisah/gembala/febrian', component: GembalaComponent,
-    data: {
-      title: 'Gembala Web CheckIn Pdt. Febrian',
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/kisah/gembala/febrian'
-    }
-  },
-  {
-    path: 'connect', component: PersekutuanComponent,
-    data: {
-      title: 'CONNECT Web CheckIn',
-      // tslint:disable-next-line:max-line-length
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/connect'
-    }
-  },
-  {
-    path: 'connect/youth', component: PersekutuanConnectComponent,
-    data: {
-      title: 'CONNECT Youth Web CheckIn',
-      // tslint:disable-next-line:max-line-length
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/connect/youth'
-    }
-  },
-  {
-    path: 'connect/connect', component: PersekutuanPwComponent,
-    data: {
-      title: 'Jadwal CONNECT Web CheckIn',
-      // tslint:disable-next-line:max-line-length
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/connect/connect'
-    }
-  },
-  {
-    path: 'komunitas', component: KomunitasComponent,
-    data: {
-      title: 'Komunitas Web CheckIn',
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/komunitas'
-    }
-  },
-  {
-    path: 'komunitas/kids', component: KomunitasKidsComponent,
-    data: {
-      title: 'Web CheckIn Kids',
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/komunitas/kids'
-    }
-  },
-  {
-    path: 'komunitas/teens', component: KomunitasTeensComponent,
-    data: {
-      title: 'Web CheckIn Teens',
-      // tslint:disable-next-line:max-line-length
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/komunitas/teens'
-    }
-  },
-  {
-    path: 'komunitas/youth', component: KomunitasYouthComponent,
-    data: {
-      title: 'Web CheckIn Youth',
-      // tslint:disable-next-line:max-line-length
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/komunitas/youth'
-    }
-  },
-  {
-    path: 'komunitas/demud', component: KomunitasDemudComponent,
-    data: {
-      title: 'Web CheckIn Dewasa Muda',
-      // tslint:disable-next-line:max-line-length
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/komunitas/demud'
-    }
-  },
-  {
-    path: 'komunitas/excellent', component: KomunitasExcellentComponent,
-    data: {
-      title: 'Web CheckIn Excellent',
-      // tslint:disable-next-line:max-line-length
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/komunitas/excellent'
-    }
-  },
-  {
-    path: 'komunitas/premium', component: KomunitasPremiumComponent,
-    data: {
-      title: 'Web CheckIn Premium',
-      // tslint:disable-next-line:max-line-length
-      description: 'Tetap absen dari HP anda saat WFH',
-      ogUrl: 'https://checkints.web.app/komunitas/premium'
     }
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,47 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
-import { KisahComponent } from './kisah/kisah.component';
 import { FacerecogComponent } from './facerecog/facerecog.component';
 import { DynamicBackgroundImageComponent } from './dynamic-background-image/dynamic-background-image.component';
 import { OverlayMenuComponent } from './overlay-menu/overlay-menu.component';
 import { ModuleVerticalTitleComponent } from './module-vertical-title/module-vertical-title.component';
 import { ModuleMenuComponent } from './module-menu/module-menu.component';
-import { GembalaComponent } from './gembala/gembala.component';
-import { PersekutuanComponent } from './persekutuan/persekutuan.component';
-import { PersekutuanConnectComponent } from './persekutuan-connect/persekutuan-connect.component';
-import { PersekutuanPwComponent } from './persekutuan-pw/persekutuan-pw.component';
-import { KomunitasComponent } from './komunitas/komunitas.component';
-import { KomunitasKidsComponent } from './komunitas-kids/komunitas-kids.component';
-import { KomunitasTeensComponent } from './komunitas-teens/komunitas-teens.component';
-import { KomunitasYouthComponent } from './komunitas-youth/komunitas-youth.component';
-import { KomunitasDemudComponent } from './komunitas-demud/komunitas-demud.component';
-import { KomunitasExcellentComponent } from './komunitas-excellent/komunitas-excellent.component';
-import { KomunitasPremiumComponent } from './komunitas-premium/komunitas-premium.component';
-import { VisiComponent } from './visi/visi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    KisahComponent,
     FacerecogComponent,
     DynamicBackgroundImageComponent,
     OverlayMenuComponent,
     ModuleVerticalTitleComponent,
-    ModuleMenuComponent,
-    GembalaComponent,
-    PersekutuanComponent,
-    PersekutuanConnectComponent,
-    PersekutuanPwComponent,
-    KomunitasComponent,
-    KomunitasKidsComponent,
-    KomunitasTeensComponent,
-    KomunitasYouthComponent,
-    KomunitasDemudComponent,
-    KomunitasExcellentComponent,
-    KomunitasPremiumComponent,
-    VisiComponent
+    ModuleMenuComponent
   ],
   imports: [
     BrowserModule,
